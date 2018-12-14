934037163e9142e2623da4f0406bdc7591fc43f5
Braxton DeHate <braxton.d@digipen.edu>
2018-12-14 15:33:56 -0800

    Started on a few scripts!

---
+++ ExtraCreditsXmasJam/Assets/Scripts/AttachmentPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachmentPoint : MonoBehaviour
+{
+
+    public Vector2 AttachmentDirection = new Vector2(1.0f, 0.0f);
+
+    public float gizmoLength = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position +
+            new Vector3(AttachmentDirection.normalized.x * gizmoLength, AttachmentDirection.normalized.y * gizmoLength, 0.0f));
+    }
+    
+}
