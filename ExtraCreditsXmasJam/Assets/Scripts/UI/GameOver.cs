02ac47e3eb60e06866a78b64261f1c9d265bd600
Devone Reynolds <mtae000@gmail.com>
2018-12-17 18:47:31 -0800

    Added UI scripts and animations

---
+++ ExtraCreditsXmasJam/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Text
+
+public class GameOver : MonoBehaviour
+{
+
+    //Amount of health the player has
+    private int _health = 20;
+
+    //Text that displays the player's health
+    public Text healthText;
+
+    //Amount of time passed from 0 ~ 1 second
+    private float _timePassed = 0;
+
+    private void Update()
+    {
+
+        //Keep track of the amount of time passed
+        _timePassed += Time.deltaTime;
+
+        //Convert the player's health into a string
+        healthText.text = "Game Over: " + _health;
+
+        //Check if one second has passed
+        if (Countdown())
+        {
+            //Decrement the player's health
+            --_health;
+
+            //Clamp health to zero
+            if (_health < 0)
+            {
+                _health = 0;
+            }
+
+        }
+
+        //Check if the user has pressed the spacebar key
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Decrement the player's health
+            --_health;
+
+            //Clamp health to zero
+            if (_health < 0)
+            {
+                _health = 0;
+            }
+        }
+
+    }
+
+    private bool Countdown()
+    {
+        //Check if one second has passed
+        if (_timePassed > 1)
+        {
+            //Reset the time passed and return that one second has passed
+            _timePassed = 0;
+            return true;
+        }
+
+        //Return that one second has NOT passed
+        return false;
+    }
+}
---
+++ ExtraCreditsXmasJam/Assets/Scripts/UI/Timed_UI_Screen.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UserInt
+{
+public class Timed_UI_Screen : UI_Screen {
+
+        #region MAIN_VARS
+		[Header("Timed Screen Properties")]
+		public float screenTime = 2.0f;
+
+		[Header("Timed Screen Events")]
+		public UnityEvent onTimeCompleted = new UnityEvent();
+
+        #endregion
+
+        #region HELPER_VARS
+		private float startTime;
+        #endregion
+
+        #region MAIN_METHODS
+		// NAME   : StartScreen
+        // DESC   : Displays the screen and starts its logic
+        // PARAMS : N/A
+        // RETURN : N/A
+		public override void StartScreen()
+		{
+			// Start the screen
+			base.StartScreen();
+
+			// Set the start time to the current time
+			startTime = Time.time;
+
+			// Wait for the time to run out
+			StartCoroutine(WaitForTime());
+		}
+        #endregion
+
+        #region HELPER_METHODS
+
+		// NAME   : WaitForTime
+        // DESC   : Halts processes on this object until the timer has run out
+        // PARAMS : N/A
+        // RETURN : TBD
+		private IEnumerator WaitForTime()
+		{
+			// Wait for the time to run ou
+			yield return new WaitForSeconds(screenTime);
+
+			// Broadcast that the timer has completed
+			if(onTimeCompleted != null)
+			{
+				onTimeCompleted.Invoke();
+			}
+		}
+        #endregion
+}
+}
+
